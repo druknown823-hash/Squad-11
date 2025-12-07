5fd59f68e9141894082056cf4691b25b214d85ca
kushagrakhatri2007-blip <kushagra.khatri2007@gmail.com>
2025-12-08 02:36:30 +0530

    Create 26.custom python module .py

---
+++ 26.custom python module .py
@@ -0,0 +1,10 @@
+# User define Module : my_module.py
+def greeting(name):
+    return f"hello,{name}!"
+def farewell(name):
+    return f"goodbye,{name}!"
+def add(a,b):
+    return a+b
+def substraction(a,b):
+    return a-b
+     
