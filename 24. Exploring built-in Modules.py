485715fb33f7391ff75d188fcd22328095c06c55
kushagrakhatri2007-blip <kushagra.khatri2007@gmail.com>
2025-12-08 02:28:13 +0530

    Create 24. Exploring built-in Modules.py

---
+++ 24. Exploring built-in Modules.py
@@ -0,0 +1,10 @@
+#explore math module
+import math
+print(math.sqrt(16))
+print(math.factorial(5))
+print(math.pi)
+print(math.sin(math.pi/2))
+print(math.log(100,10))
+print(math.ceil(4.3))
+print(math.floor(4.7))
+print(math.gcd(48,18))
