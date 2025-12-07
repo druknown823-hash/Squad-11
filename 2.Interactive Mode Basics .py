3c65efdf0dd078d4ab4b8f0bc2b10376aeb421eb
druknown823-hash <druknown823@gmail.com>
2025-12-08 00:47:36 +0530

    Update 2.Interactive Mode Basics .py

---
+++ 2.Interactive Mode Basics .py
@@ -0,0 +1,5 @@
+5 + 3 # Python adds 5 and 3
+10 - 4 # Python subtracts 4 from 10
+6 * 7 # Python multiplies 6 and 7
+10 / 3 # Python divides 10 by 3, giving a precise decimal result
+10 // 3 # Python divides 10 by 3 and only keeps the whole number part
